95c347b83432afc9c48babe5578f8e4ca07ca339
Shahnawaz khan <74444644+Shahnawazk7944@users.noreply.github.com>
2024-02-08 23:36:33 +0530

    lottie animation added

--- app/src/main/java/com/example/ai_chat_application_zeru_with_mvvm_retrofit_room/MainActivity.kt
+++ app/src/main/java/com/example/ai_chat_application_zeru_with_mvvm_retrofit_room/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.ai_chat_application_zeru_with_mvvm_retrofit_room.Screens.SplashScreen
 import com.example.ai_chat_application_zeru_with_mvvm_retrofit_room.ui.theme.AI_Chat_Application_ZERU_With_MVVM_Retrofit_ROOMTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    SplashScreen()
                 }
             }
         }
---
+++ app/src/main/java/com/example/ai_chat_application_zeru_with_mvvm_retrofit_room/Screens/SplashScreen.kt
@@ -0,0 +1,54 @@
+package com.example.ai_chat_application_zeru_with_mvvm_retrofit_room.Screens
+
+import android.window.SplashScreen
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieComposition
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.example.ai_chat_application_zeru_with_mvvm_retrofit_room.R
+
+@Composable
+fun SplashScreen() {
+    val composition by rememberLottieComposition(
+        spec = LottieCompositionSpec.RawRes(R.raw.splash_screen)
+    )
+    var isPlaying by remember { mutableStateOf(true) }
+
+    val progress by animateLottieCompositionAsState(
+        composition = composition, isPlaying = isPlaying
+    )
+    LaunchedEffect(key1 = progress) {
+        if (progress == 0f) {
+            isPlaying = true
+        }
+        if (progress == 1f) {
+            isPlaying = false
+        }
+    }
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            LottieAnimation(
+                composition = composition, progress = { progress }, modifier = Modifier
+                    .size(400.dp)
+                    .align(Alignment.Center)
+            )
+        }
+    }
+
+}
