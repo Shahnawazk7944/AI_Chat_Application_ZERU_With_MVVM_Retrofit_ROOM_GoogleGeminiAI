fdcd35f043a6f0215ca153a107a2a31b9cea366d
Shahnawaz khan <74444644+Shahnawazk7944@users.noreply.github.com>
2024-02-14 02:33:00 +0530

    onBoarding screen done
    Co-Authored-By: Iycs2122 <154696547+iycs2122@users.noreply.github.com>

--- app/src/main/java/com/example/ai_chat_application_zeru_with_mvvm_retrofit_room/MainActivity.kt
+++ app/src/main/java/com/example/ai_chat_application_zeru_with_mvvm_retrofit_room/MainActivity.kt
@@ -3,26 +3,18 @@ package com.example.ai_chat_application_zeru_with_mvvm_retrofit_room
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.ai_chat_application_zeru_with_mvvm_retrofit_room.navigation.NavigationGraph
 import com.example.ai_chat_application_zeru_with_mvvm_retrofit_room.ui.theme.AI_Chat_Application_ZERU_With_MVVM_Retrofit_ROOMTheme
-import com.example.ai_chat_application_zeru_with_mvvm_retrofit_room.ui.theme.PrimaryColor
 
 class MainActivity : ComponentActivity() {
     private lateinit var navController: NavHostController
@@ -39,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     navController = rememberNavController()
-                    NavigationGraph(navController = navController)
+                    NavigationGraph(navController = navController, context = this@MainActivity)
 
                 }
             }
