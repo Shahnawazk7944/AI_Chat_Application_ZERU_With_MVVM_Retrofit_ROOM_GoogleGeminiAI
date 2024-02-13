adbdf648aeddaea6a9ab077845a4ae601175bf95
Shahnawaz khan <74444644+Shahnawazk7944@users.noreply.github.com>
2024-02-14 02:54:01 +0530

    auth implemention started
    Co-Authored-By: Iycs2122 <154696547+iycs2122@users.noreply.github.com>

--- app/src/main/java/com/example/ai_chat_application_zeru_with_mvvm_retrofit_room/navigation/Screen.kt
+++ app/src/main/java/com/example/ai_chat_application_zeru_with_mvvm_retrofit_room/navigation/Screen.kt
@@ -3,4 +3,6 @@ package com.example.ai_chat_application_zeru_with_mvvm_retrofit_room.navigation
 sealed class Screen(val route: String) {
     object Welcome: Screen(route = "welcomeScreen")
     object Home: Screen(route = "home")
+    object SignIn: Screen(route = "signIn")
+    object SignUp: Screen(route = "signUp")
 }
