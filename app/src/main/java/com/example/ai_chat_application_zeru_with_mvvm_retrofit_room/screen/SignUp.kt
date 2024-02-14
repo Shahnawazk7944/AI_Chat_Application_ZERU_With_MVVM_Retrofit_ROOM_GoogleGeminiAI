56d105c8c4d26ad15afe39eebae61ce07ee627f0
Shahnawaz khan <74444644+Shahnawazk7944@users.noreply.github.com>
2024-02-14 20:41:12 +0530

    Create Account UI started
    Co-Authored-By: Iycs2122 <154696547+iycs2122@users.noreply.github.com>

--- app/src/main/java/com/example/ai_chat_application_zeru_with_mvvm_retrofit_room/screen/SignUp.kt
+++ app/src/main/java/com/example/ai_chat_application_zeru_with_mvvm_retrofit_room/screen/SignUp.kt
@@ -1,48 +1,214 @@
 package com.example.ai_chat_application_zeru_with_mvvm_retrofit_room.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import com.example.ai_chat_application_zeru_with_mvvm_retrofit_room.navigation.Screen
+import androidx.navigation.compose.rememberNavController
+import com.example.ai_chat_application_zeru_with_mvvm_retrofit_room.R
+import com.example.ai_chat_application_zeru_with_mvvm_retrofit_room.ui.theme.GrayColor
+import com.example.ai_chat_application_zeru_with_mvvm_retrofit_room.ui.theme.LightGrayColor
 import com.example.ai_chat_application_zeru_with_mvvm_retrofit_room.ui.theme.PrimaryBackground
+import com.example.ai_chat_application_zeru_with_mvvm_retrofit_room.ui.theme.PrimaryColor
 import com.example.ai_chat_application_zeru_with_mvvm_retrofit_room.ui.theme.PrimaryFontColor
+import com.example.ai_chat_application_zeru_with_mvvm_retrofit_room.ui.theme.poppins
 import com.example.ai_chat_application_zeru_with_mvvm_retrofit_room.ui.theme.ubuntu
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SignUp(navController: NavHostController){
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(PrimaryBackground),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(text = "Sign Up Screen",
-            fontWeight = FontWeight.Medium,
-            fontSize = 30.sp,
-            fontFamily = ubuntu,
-            color = PrimaryFontColor
-        )
-        Spacer(modifier = Modifier.height(10.dp))
-        Text(text = "Already have Account?",
-            fontWeight = FontWeight.Medium,
-            fontSize = 20.sp,
-            fontFamily = ubuntu,
-            color = Color.Blue,
-            modifier = Modifier.clickable { navController.navigate(Screen.SignIn.route) }
-        )
+fun SignUp(navController: NavHostController) {
+    var email by remember {
+        mutableStateOf("")
     }
+    var password by remember {
+        mutableStateOf("")
+    }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {},
+                navigationIcon = {
+                    IconButton(onClick = { navController.navigateUp() }) {
+                        Image(
+                            painter = painterResource(R.drawable.arrow_back),
+                            contentDescription = "back Icon", Modifier.size(18.dp)
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = PrimaryBackground
+                )
+            )
+        },
+
+        ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(PrimaryBackground)
+                .padding(paddingValues)
+                .padding(20.dp),
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Top
+        ) {
+
+            Text(
+                "Create your Account",
+                fontFamily = ubuntu,
+                fontSize = 50.sp,
+                fontWeight = FontWeight.Medium,
+                lineHeight = 60.sp,
+                color = PrimaryFontColor,
+                textAlign = TextAlign.Start,
+                modifier = Modifier
+            )
+            Spacer(modifier = Modifier.height(60.dp))
+            OutlinedTextField(
+                value = email,
+                onValueChange = {
+                    email = it
+                },
+                textStyle = TextStyle(
+                    fontFamily = ubuntu,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Normal,
+                    color = PrimaryFontColor
+                ),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Email
+                ),
+                singleLine = true,
+                placeholder = {
+                    Text(
+                        text = "Email",
+                        fontFamily = ubuntu,
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Normal,
+                        color = GrayColor
+                    )
+                },
+                shape = RoundedCornerShape(15.dp),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedContainerColor = Color(0x1700CDBD),
+                    unfocusedContainerColor = Color(0xD8DFF5F3),
+                    unfocusedBorderColor = Color.Transparent,
+                    focusedBorderColor = PrimaryColor,
+                    focusedLeadingIconColor = PrimaryColor,
+                    unfocusedLeadingIconColor = PrimaryFontColor
+                ),
+                leadingIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.email),
+                            contentDescription = "email icon",
+                            modifier = Modifier.size(25.dp)
+                        )
+                    }
+                },
+                modifier = Modifier.fillMaxWidth(),
+            )
+
+
+            Spacer(modifier = Modifier.height(20.dp))
+            OutlinedTextField(
+                value = password,
+                onValueChange = {
+                    password = it
+                },
+                textStyle = TextStyle(
+                    fontFamily = ubuntu,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Normal,
+                    color = PrimaryFontColor
+                ),
+
+                placeholder = {
+                    Text(
+                        text = "Password",
+                        fontFamily = ubuntu,
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Normal,
+                        color = GrayColor
+                    )
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password
+                ),
+                singleLine = true,
+                shape = RoundedCornerShape(15.dp),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedContainerColor = Color(0x1700CDBD),
+                    unfocusedContainerColor = Color(0xD8DFF5F3),
+                    unfocusedBorderColor = Color.Transparent,
+                    focusedBorderColor = PrimaryColor,
+                    focusedLeadingIconColor = PrimaryColor,
+                    unfocusedLeadingIconColor = PrimaryFontColor,
+                    focusedTrailingIconColor = PrimaryColor,
+                    unfocusedTrailingIconColor = PrimaryFontColor
+                ),
+                leadingIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.password),
+                            contentDescription = "email icon",
+                            modifier = Modifier.size(25.dp)
+                        )
+                    }
+                },
+                trailingIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.invisible),
+                            contentDescription = "email icon",
+                            modifier = Modifier.size(25.dp)
+                        )
+                    }
+                },
+
+                modifier = Modifier.fillMaxWidth(),
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SignUpPreview() {
+    SignUp(navController = rememberNavController())
 }
