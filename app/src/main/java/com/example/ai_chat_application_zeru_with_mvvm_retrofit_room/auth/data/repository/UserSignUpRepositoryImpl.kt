c31e64944ed2cdebb453219d9fc74a8fb2dffab4
Shahnawaz khan <74444644+Shahnawazk7944@users.noreply.github.com>
2024-02-20 20:51:41 +0530

    solved some errors
    Co-Authored-By: Iycs2122 <154696547+Iycs2122@users.noreply.github.com>

--- app/src/main/java/com/example/ai_chat_application_zeru_with_mvvm_retrofit_room/auth/data/repository/UserSignUpRepositoryImpl.kt
+++ app/src/main/java/com/example/ai_chat_application_zeru_with_mvvm_retrofit_room/auth/data/repository/UserSignUpRepositoryImpl.kt
@@ -1,20 +1,34 @@
 package com.example.ai_chat_application_zeru_with_mvvm_retrofit_room.auth.data.repository
 
-import androidx.datastore.core.DataStore
+import android.util.Log
 import arrow.core.Either
 import com.example.ai_chat_application_zeru_with_mvvm_retrofit_room.auth.domain.model.Errors
-import com.example.ai_chat_application_zeru_with_mvvm_retrofit_room.auth.domain.model.UserSignUp
 import com.example.ai_chat_application_zeru_with_mvvm_retrofit_room.auth.domain.repository.UserSignUpRepository
+import com.example.ai_chat_application_zeru_with_mvvm_retrofit_room.auth.mapper.toAuthError
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
 import javax.inject.Inject
 
 class UserSignUpRepositoryImpl @Inject constructor(
     private val auth: FirebaseAuth,
-   // private val dataStore: DataStore<UserSignUp>
-):UserSignUpRepository{
-    override suspend fun signUp(name: String, email: String, password: String): Either<Errors, Boolean> {
-        val task = auth.createUserWithEmailAndPassword(email,password)
-        return task.isSuccessful
+    // private val dataStore: DataStore<UserSignUp>
+) : UserSignUpRepository {
+    override suspend fun signUp(
+        name: String,
+        email: String,
+        password: String
+    ): Either<Errors, Boolean> {
+        Log.d("check2", "function called")
+        return try {
+            val result = auth.createUserWithEmailAndPassword(email, password)
+            try {
+                Either.Right(result.isSuccessful)
+            } catch (e: FirebaseAuthException) {
+                Either.Left(e.toAuthError())
+            }
+        } catch (e: FirebaseAuthException) {
+            Either.Left(e.toAuthError())
+        }
     }
 
 }
