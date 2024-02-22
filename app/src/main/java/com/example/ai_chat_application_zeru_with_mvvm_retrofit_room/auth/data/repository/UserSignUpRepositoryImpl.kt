c015a7a6a559a2d3155a0bbe06df159057487d8d
Shahnawaz khan <74444644+Shahnawazk7944@users.noreply.github.com>
2024-02-22 16:49:45 +0530

    added some exception handlings
    Co-Authored-By: Iycs2122 <154696547+Iycs2122@users.noreply.github.com>

--- app/src/main/java/com/example/ai_chat_application_zeru_with_mvvm_retrofit_room/auth/data/repository/UserSignUpRepositoryImpl.kt
+++ app/src/main/java/com/example/ai_chat_application_zeru_with_mvvm_retrofit_room/auth/data/repository/UserSignUpRepositoryImpl.kt
@@ -7,6 +7,7 @@ import com.example.ai_chat_application_zeru_with_mvvm_retrofit_room.auth.domain.
 import com.example.ai_chat_application_zeru_with_mvvm_retrofit_room.auth.mapper.toAuthError
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthException
+import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
 class UserSignUpRepositoryImpl @Inject constructor(
@@ -21,17 +22,11 @@ class UserSignUpRepositoryImpl @Inject constructor(
         Log.e("check2", "function called")
         return try {
             Log.e("check2", "enter in first try")
-            val result = auth.createUserWithEmailAndPassword(email, password)
-            Log.d("check after result", "${result.isSuccessful}")
-            try {
-                Log.d("check in try", "${result.isSuccessful}")
-                Either.Right(result.isSuccessful)
-            } catch (e: FirebaseAuthException) {
-                Log.e("test Inner", e.errorCode)
-                Either.Left(e.toAuthError())
-            }
+            val result = auth.createUserWithEmailAndPassword(email, password).await()
+                Log.d("check in try", "${result.user != null}")
+                Either.Right(result.user != null)
         } catch (e: FirebaseAuthException) {
-            Log.e("test", e.errorCode)
+            Log.e("test main", e.errorCode)
             Either.Left(e.toAuthError())
         }
     }
--- app/src/main/java/com/example/ai_chat_application_zeru_with_mvvm_retrofit_room/auth/mapper/Mapper.kt
+++ app/src/main/java/com/example/ai_chat_application_zeru_with_mvvm_retrofit_room/auth/mapper/Mapper.kt
@@ -4,12 +4,11 @@ import com.example.ai_chat_application_zeru_with_mvvm_retrofit_room.auth.domain.
 import com.example.ai_chat_application_zeru_with_mvvm_retrofit_room.auth.domain.model.Errors
 import com.google.firebase.auth.FirebaseAuthException
 
-fun Throwable.toAuthError(): Errors {
-    val error = when (this.message) {
+fun FirebaseAuthException.toAuthError(): Errors {
+    val error = when (this.errorCode) {
         "NETWORK_ERROR" -> AuthError.NetworkConnectionError
-        "WEAK_PASSWORD" -> AuthError.WeakPassword
-        "EMAIL_EXISTS" -> AuthError.EmailExist
-        "INVALID_PASSWORD" -> AuthError.InvalidPassword
+        "ERROR_EMAIL_ALREADY_IN_USE" -> AuthError.EmailExist
+        "ERROR_INVALID_CREDENTIAL" -> AuthError.InvalidCredential
         "TOO_MANY_ATTEMPTS" -> AuthError.TooManyAttempt
         "ERROR_USER_NOT_FOUND" -> AuthError.UserNotFound
         "ERROR_USER_DISABLED" -> AuthError.UserDisable
